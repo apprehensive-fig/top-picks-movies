5f37a081089ab7c6f20eb71dfe79ae92d07198ff
alexis sae an <alexis.sae.an@gmail.com>
2021-05-20 18:13:00 +0900

    updated delete

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :lists, only: [:index, :show, :new, :create] do
-    resources :bookmarks, only: [:show, :new, :create, :destroy]
+    resources :bookmarks, only: [:show, :new, :create]
   end
+    resources :bookmarks, only: [:destroy]
 end
