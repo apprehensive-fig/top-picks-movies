6ac8ce095e12a0bcff9cc3bd612768254fdaa044
alexis sae an <alexis.sae.an@gmail.com>
2021-05-21 12:55:51 +0900

    updated photo features

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  root to: 'lists#index'
+
   resources :lists, only: [:index, :show, :new, :create] do
     resources :bookmarks, only: [:show, :new, :create]
   end
-    resources :bookmarks, only: [:destroy]
+
+  resources :bookmarks, only: [:destroy]
+
 end
