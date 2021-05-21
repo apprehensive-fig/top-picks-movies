4dcbe02dd067e546c4a2ae2fc7c9f8d3456e1d8a
alexis sae an <alexis.sae.an@gmail.com>
2021-05-21 18:38:49 +0900

    updated sweetalert

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,12 +17,11 @@ import 'bootstrap';
 // const imagePath = (name) => images(name, true)
 import { initSweetalert } from '../plugins/init_sweetalert';
 
-document.addEventListener('turbolinks:load', () => {
-  initSweetalert('#sweet-alert-demo', {
-    title: "are you sure?",
-  }, (value) => {
-    if (value) {
-      const link = document.querySelector('#delete-link');
-    }
-  });
+initSweetalert('#sweet-alert-demo', {
+  title: "are you sure?",
+}, (value) => {
+  if (value) {
+    const link = document.querySelector('#delete-link');
+    link.click();
+  }
 });
